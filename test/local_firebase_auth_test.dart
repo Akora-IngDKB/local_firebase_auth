5b7e778c39bda5c793e69794b24c6d0b928e9b9a
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-22 21:13:32 +0000

    Initial commit

---
+++ test/local_firebase_auth_test.dart
@@ -0,0 +1,12 @@
+import 'package:local_firebase_auth/local_firebase_auth.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
