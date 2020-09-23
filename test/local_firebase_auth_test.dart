2de86458b9b2873b722f25e6281de8e2e004e101
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-23 00:36:21 +0000

    Sample tests

--- test/local_firebase_auth_test.dart
+++ test/local_firebase_auth_test.dart
@@ -1,12 +1,47 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:local_firebase_auth/local_firebase_auth.dart';
-import 'package:test/test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  final _auth = LocalFirebaseAuth.instance;
+  test('User creation test', () async {
+    final user1 = await _auth.createUserWithEmailAndPassword(
+        email: 'user1@gmail.com', password: 'password');
+
+    final user3 = await _auth.signInAnonymously();
+
+    print("User 1: ${user1.user}");
+    print("User 3: ${user3.user}");
+  });
+
+  test('User sign in test', () async {
+    final user1 = await _auth.signInWithEmailAndPassword(
+        email: 'user1@gmail.com', password: 'password');
+
+    try {
+      final noUser = await _auth.signInWithEmailAndPassword(
+          email: 'nouser@gmail.com', password: 'password');
+
+      print("Sign In No User: ${noUser.user}");
+    } on FirebaseAuthException catch (e) {
+      print(e.toString());
+    } catch (e) {}
+
+    print("Sign In User 1: ${user1.user}");
+  });
+
+  test('Current user test', () {
+    final currentUser = _auth.currentUser;
+
+    print("Current User: $currentUser");
+  });
+
+  test('Sign Out test', () async {
+    await _auth.signOut();
+  });
+
+  test('Current user test 2', () {
+    final currentUser = _auth.currentUser;
+
+    print("Current User: $currentUser");
   });
 }
