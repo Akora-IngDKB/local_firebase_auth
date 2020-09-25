a0b5042b5c36ada9243790829a45ab97cbd241fb
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-25 20:22:37 +0000

    Written cleaner tests

--- test/local_firebase_auth_test.dart
+++ test/local_firebase_auth_test.dart
@@ -5,13 +5,33 @@ void main() {
   LocalFirebaseAuth.initialize('testApp');
   final _auth = LocalFirebaseAuth.instance;
 
-  test('User creation test', () async {
+  test('Create User Test', () async {
     final email = 'user@gmail.com';
     final pass = 'password';
+
     final user = await _auth.createUserWithEmailAndPassword(
-        email: email, password: pass);
+      email: email,
+      password: pass,
+    );
 
     expect(user.user.email, email);
+  });
+
+  test('Create User Test: User Exists', () async {
+    final email = 'user@gmail.com';
+    final pass = 'password';
+
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: pass,
+      );
+
+      // This test is expected to fail.
+      // I'm using try-catch for the sake of Github Workflow
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+    }
   });
 
   test('Anonymous Sign In Test', () async {
@@ -20,35 +40,34 @@ void main() {
     expect(ann.user.email, null);
   });
 
-  // test('User sign in test', () async {
-  //   final user1 = await _auth.signInWithEmailAndPassword(
-  //       email: 'user1@gmail.com', password: 'password');
+  test('Sign In Test: No Existing User', () async {
+    final email = 'nouser@gmail.com';
+    final pass = 'password';
 
-  //   try {
-  //     final noUser = await _auth.signInWithEmailAndPassword(
-  //         email: 'nouser@gmail.com', password: 'password');
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: pass);
 
-  //     print("Sign In No User: ${noUser.user}");
-  //   } on FirebaseAuthException catch (e) {
-  //     print(e.toString());
-  //   } catch (e) {}
+      // This test is expected to fail.
+      // I'm using try-catch for the sake of Github Workflow
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+    }
+  });
 
-  //   print("Sign In User 1: ${user1.user}");
-  // });
+  test('Current User Test', () {
+    final user = _auth.currentUser;
 
-  // test('Current user test', () {
-  //   final currentUser = _auth.currentUser;
+    expect(user, isNotNull);
+  });
 
-  //   print("Current User: $currentUser");
-  // });
+  test('Sign Out Test', () async {
+    await _auth.signOut();
+  });
 
-  // test('Sign Out test', () async {
-  //   await _auth.signOut();
-  // });
+  test('Current User Test', () {
+    final user = _auth.currentUser;
 
-  // test('Current user test 2', () {
-  //   final currentUser = _auth.currentUser;
-
-  //   print("Current User: $currentUser");
-  // });
+    // Expect no user after signing out.
+    expect(user, isNull);
+  });
 }
