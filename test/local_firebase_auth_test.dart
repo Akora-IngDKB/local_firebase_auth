9e4b7c349f4a404ef467b32e8c0cfcec9bf93dc6
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-25 17:27:07 +0000

    Sample test for hive storage [registration]

--- test/local_firebase_auth_test.dart
+++ test/local_firebase_auth_test.dart
@@ -2,6 +2,7 @@ import 'package:local_firebase_auth/local_firebase_auth.dart';
 import 'package:test/test.dart';
 
 void main() {
+  LocalFirebaseAuth.initialize('testApp');
   final _auth = LocalFirebaseAuth.instance;
 
   test('User creation test', () async {
@@ -14,35 +15,35 @@ void main() {
     print("User 3: ${user3.user}");
   });
 
-  test('User sign in test', () async {
-    final user1 = await _auth.signInWithEmailAndPassword(
-        email: 'user1@gmail.com', password: 'password');
+  // test('User sign in test', () async {
+  //   final user1 = await _auth.signInWithEmailAndPassword(
+  //       email: 'user1@gmail.com', password: 'password');
 
-    try {
-      final noUser = await _auth.signInWithEmailAndPassword(
-          email: 'nouser@gmail.com', password: 'password');
+  //   try {
+  //     final noUser = await _auth.signInWithEmailAndPassword(
+  //         email: 'nouser@gmail.com', password: 'password');
 
-      print("Sign In No User: ${noUser.user}");
-    } on FirebaseAuthException catch (e) {
-      print(e.toString());
-    } catch (e) {}
+  //     print("Sign In No User: ${noUser.user}");
+  //   } on FirebaseAuthException catch (e) {
+  //     print(e.toString());
+  //   } catch (e) {}
 
-    print("Sign In User 1: ${user1.user}");
-  });
+  //   print("Sign In User 1: ${user1.user}");
+  // });
 
-  test('Current user test', () {
-    final currentUser = _auth.currentUser;
+  // test('Current user test', () {
+  //   final currentUser = _auth.currentUser;
 
-    print("Current User: $currentUser");
-  });
+  //   print("Current User: $currentUser");
+  // });
 
-  test('Sign Out test', () async {
-    await _auth.signOut();
-  });
+  // test('Sign Out test', () async {
+  //   await _auth.signOut();
+  // });
 
-  test('Current user test 2', () {
-    final currentUser = _auth.currentUser;
+  // test('Current user test 2', () {
+  //   final currentUser = _auth.currentUser;
 
-    print("Current User: $currentUser");
-  });
+  //   print("Current User: $currentUser");
+  // });
 }
