cf64b4740cc3c2be3893c2fcd6c033022a11bef4
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-26 18:28:26 +0000

    Added deletion test and Grouped similar tests

--- test/local_firebase_auth_test.dart
+++ test/local_firebase_auth_test.dart
@@ -5,116 +5,168 @@ void main() {
   LocalFirebaseAuth.initialize('testApp');
   final _auth = LocalFirebaseAuth.instance;
 
-  test('Create User Test', () async {
-    final email = 'user@gmail.com';
-    final pass = 'password';
+  group('User Creation Tests:', () {
+    test('Create User Test', () async {
+      final email = 'user@gmail.com';
+      final pass = 'password';
 
-    final user = await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: pass,
-    );
-
-    expect(user.user.email, email);
-  });
-
-  test('Create User Test: User Already Exists', () async {
-    final email = 'user@gmail.com';
-    final pass = 'password';
-
-    try {
-      await _auth.createUserWithEmailAndPassword(
+      final credential = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: pass,
       );
 
-      // This test is expected to fail.
-      // I'm using try-catch for the sake of Github Workflow
-    } on FirebaseAuthException catch (e) {
-      expect(e.code, 'ERROR_EMAIL_ALREADY_IN_USE');
-    }
+      expect(credential.user.email, email);
+    });
+
+    test('Create User Test: User Already Exists', () async {
+      final email = 'user@gmail.com';
+      final pass = 'password';
+
+      try {
+        await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: pass,
+        );
+
+        // This test is expected to fail.
+        // I'm using try-catch for the sake of Github Workflow.
+      } on FirebaseAuthException catch (e) {
+        expect(e.code, 'ERROR_EMAIL_ALREADY_IN_USE');
+      }
+    });
+
+    test('Create User Test: Invalid Email', () async {
+      final email = 'gmail.com';
+      final pass = 'password';
+
+      try {
+        await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: pass,
+        );
+
+        // This test is expected to fail.
+        // I'm using try-catch for the sake of Github Workflow.
+      } on FirebaseAuthException catch (e) {
+        expect(e.code, 'ERROR_INVALID_EMAIL');
+      }
+    });
   });
 
-  test('Create User Test: Invalid Email', () async {
-    final email = 'gmail.com';
-    final pass = 'password';
+  group('Anonymous User Tests:', () {
+    User ann1;
+    test('Sign In Test', () async {
+      final ann = await _auth.signInAnonymously();
 
-    try {
-      await _auth.createUserWithEmailAndPassword(
+      ann1 = ann.user;
+      expect(ann.user.email, null);
+    });
+
+    test('', () async {
+      final ann2 = await _auth.signInAnonymously();
+
+      // Currently logged in user is anonymous.
+      // We expect the same user to be returned.
+      expect(ann2.user.uid, ann1.uid);
+    });
+  });
+
+  group('Sign In Tests:', () {
+    test('Sign In Test', () async {
+      final email = 'user@gmail.com';
+      final pass = 'password';
+
+      final credential = await _auth.signInWithEmailAndPassword(
         email: email,
         password: pass,
       );
 
-      // This test is expected to fail.
-      // I'm using try-catch for the sake of Github Workflow
-    } on FirebaseAuthException catch (e) {
-      expect(e.code, 'ERROR_INVALID_EMAIL');
-    }
-  });
+      expect(credential.user.email, email);
+    });
 
-  test('Anonymous Sign In Test', () async {
-    final ann = await _auth.signInAnonymously();
+    test('Sign In Test: Wrong Password', () async {
+      final email = 'user@gmail.com';
+      final pass = 'passwords';
 
-    expect(ann.user.email, null);
+      try {
+        await _auth.signInWithEmailAndPassword(email: email, password: pass);
 
-    final ann2 = await _auth.signInAnonymously();
+        // This test is expected to fail.
+        // I'm using try-catch for the sake of Github Workflow.
+      } on FirebaseAuthException catch (e) {
+        expect(e.code, 'ERROR_WRONG_PASSWORD');
+      }
+    });
 
-    expect(ann2.user.uid, ann.user.uid);
-  });
-
-  test('Sign In Test', () async {
-    final email = 'user@gmail.com';
-    final pass = 'password';
-
-    final user = await _auth.signInWithEmailAndPassword(
-      email: email,
-      password: pass,
-    );
-
-    expect(user.user.email, email);
-  });
-
-  test('Sign In Test: Wrong Password', () async {
-    final email = 'user@gmail.com';
-    final pass = 'passwords';
-
-    try {
-      await _auth.signInWithEmailAndPassword(email: email, password: pass);
+    test('Sign In Test: No Existing User', () async {
+      final email = 'nouser@gmail.com';
+      final pass = 'password';
 
       // This test is expected to fail.
-      // I'm using try-catch for the sake of Github Workflow
-    } on FirebaseAuthException catch (e) {
-      expect(e.code, 'ERROR_WRONG_PASSWORD');
-    }
+      // I'm using try-catch for the sake of Github Workflow.
+      try {
+        await _auth.signInWithEmailAndPassword(email: email, password: pass);
+      } on FirebaseAuthException catch (e) {
+        expect(e.code, 'ERROR_USER_NOT_FOUND');
+      }
+    });
   });
 
-  test('Sign In Test: No Existing User', () async {
-    final email = 'nouser@gmail.com';
-    final pass = 'password';
+  group('Current User/Sign Out Tests:', () {
+    test('Current User Test', () {
+      final user = _auth.currentUser;
 
-    try {
-      await _auth.signInWithEmailAndPassword(email: email, password: pass);
+      expect(user, isNotNull);
+    });
 
+    test('Sign Out Test', () async {
+      await _auth.signOut();
+    });
+
+    test('Current User Test', () {
+      final user = _auth.currentUser;
+
+      // Expect no user after signing out.
+      expect(user, isNull);
+    });
+  });
+
+  group('User Deletion Tests:', () {
+    User user;
+
+    test('Delete Anonymous User', () async {
+      final ann = await _auth.signInAnonymously();
+
+      // This test will fail when there's an exception.
+      await ann.user.delete();
+
+      expect(_auth.currentUser, isNull);
+    });
+
+    test('Delete Non-anonymous User', () async {
+      final email = 'user@gmail.com';
+      final pass = 'password';
+
+      // Sign in again. Previous test signed out current user.
+      final credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: pass,
+      );
+
+      user = credential.user;
+
+      // This test will fail when there's an exception.
+      await credential.user.delete();
+    });
+
+    test('Delete Non-Signed-In Non-anonymous User', () async {
       // This test is expected to fail.
-      // I'm using try-catch for the sake of Github Workflow
-    } on FirebaseAuthException catch (e) {
-      expect(e.code, 'ERROR_USER_NOT_FOUND');
-    }
-  });
-
-  test('Current User Test', () {
-    final user = _auth.currentUser;
-
-    expect(user, isNotNull);
-  });
-
-  test('Sign Out Test', () async {
-    await _auth.signOut();
-  });
-
-  test('Current User Test', () {
-    final user = _auth.currentUser;
-
-    // Expect no user after signing out.
-    expect(user, isNull);
+      // I'm using try-catch for the sake of Github Workflow.
+      try {
+        await user.delete();
+      } on FirebaseAuthException catch (e) {
+        expect(e.code, 'ERROR_REQUIRES_RECENT_LOGIN');
+      }
+    });
   });
 }
